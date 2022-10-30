7fc129eda514e02468d3bf2aebb386c4cef66ceb
InkoHX <me@inkohx.dev>
2022-10-30 18:32:12 +0900

    スレッド作成時に送信するメッセージを変更

--- src/constants.js
+++ src/constants.js
@@ -5,17 +5,12 @@ export const forumChannels = [
     id: '1019744831855153223',
     message: ownerId =>
       [
-        bold('もう一度確認してみよう'),
-        `・${hyperlink(
-          '質問のガイドライン',
-          'https://scrapbox.io/discordjs-japan/質問のガイドライン'
-        )}`,
-        `・フォーラムチャンネルに設定された${bold('Post Guideline')}`,
+        bold('もう一度確認してみよう:'),
+        `• https://scrapbox.io/discordjs-japan/質問のガイドライン`,
+        `• フォーラムチャンネルに設定された${bold('Post Guideline')}`,
         '',
-        `質問に回答する方々は、${hyperlink(
-          bold('回答のガイドライン'),
-          'https://scrapbox.io/discordjs-japan/回答のガイドライン'
-        )}を確認しましょう！`,
+        `質問に回答する方々は、${bold('回答のガイドライン')}を確認するようにしてください。`,
+        '• https://scrapbox.io/discordjs-japan/回答のガイドライン',
         '',
         `${userMention(
           ownerId
