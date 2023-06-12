cd4f61a7a65281e0447f942b54bb6262e0d152b5
Mogyuchi <mogyuchi@mogyuchi.jp>
2023-06-12 22:16:46 +0900

    early return に変更

--- src/handleReopenNotify.js
+++ src/handleReopenNotify.js
@@ -17,23 +17,23 @@ import { ButtonStyle, ComponentType } from 'discord.js'
  */
 export async function handleReopenNotify(logger, entry, thread, setting) {
   logger.info(`"${thread.name}" (${thread.id}) has been reopened.`)
-  if (entry.executorId) {
-    const message = await thread.send({
-      content: setting.onReopen(entry.executorId),
-      components,
-    })
-    const interaction = await message
-      .awaitMessageComponent({
-        filter: i => i.user.id === entry.executorId,
-        time: 1000 * 60 * 5,
-      })
-      .catch(() => null)
-    if (interaction) {
-      await message.delete()
-      await thread.setArchived()
-    }
-  } else {
+  if (!entry.executorId) {
     await thread.send(setting.onReopen())
+    return
+  }
+  const message = await thread.send({
+    content: setting.onReopen(entry.executorId),
+    components,
+  })
+  const interaction = await message
+    .awaitMessageComponent({
+      filter: i => i.user.id === entry.executorId,
+      time: 1000 * 60 * 5,
+    })
+    .catch(() => null)
+  if (interaction) {
+    await message.delete()
+    await thread.setArchived()
   }
 }
 
